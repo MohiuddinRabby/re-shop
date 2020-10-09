a37aabbe5776acacf4690987cb4d196400050527
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-10-09 23:21:17 +0600

    reducer fix

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,50 @@
 import React, { useContext } from "react";
+import { useState } from "react";
 import { CartContext } from "../../global/CartContext";
 import { ProductContext } from "../../global/ProductContext";
+import HotProducts from "../Products/HotProducts";
 import "./Home.css";
 const Home = () => {
   const { products } = useContext(ProductContext);
   const { dispatch } = useContext(CartContext);
+  const filterHotProducts = products.filter((pd) => pd.popularity === "hot");
+  const [selectCat, setCat] = useState(products);
+  const [hotP, setHotP] = useState(true);
+  const desktopItems = () => {
+    const desktop = products.filter((pd) => pd.category === "desktop");
+    setCat(desktop);
+    setHotP(false);
+  };
+  const laptopItems = () => {
+    const laptop = products.filter((pdl) => pdl.category === "laptop");
+    setCat(laptop);
+    setHotP(false);
+  };
+  const monitorItems = () => {
+    const monitor = products.filter((pd) => pd.category === "monitor");
+    setCat(monitor);
+    setHotP(false);
+  };
+  const officeItems = () => {
+    const office = products.filter((pd) => pd.category === "office-eq");
+    setCat(office);
+    setHotP(false);
+  };
+  const gamingItems = () => {
+    const gaming = products.filter((pd) => pd.category === "gaming");
+    setCat(gaming);
+    setHotP(false);
+  };
+  const cameraItems = () => {
+    const camera = products.filter((pd) => pd.category === "camera");
+    setCat(camera);
+    setHotP(false);
+  };
+  const upsItems = () => {
+    const ups = products.filter((pd) => pd.category === "ups");
+    setCat(ups);
+    setHotP(false);
+  };
   return (
     <div>
       <ul
@@ -12,67 +52,80 @@ const Home = () => {
         id="cat-navs"
       >
         <li className="nav-item">
-          <a href="#" className="nav-link">
+          <a href="#" className="nav-link" onClick={desktopItems}>
             Desktop
           </a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
+        <li className="nav-item" onClick={monitorItems}>
+          <a href="#" className="nav-link">
             Monitor
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
+          <a href="#" className="nav-link" onClick={laptopItems}>
             Laptop
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
+          <a href="#" className="nav-link" onClick={officeItems}>
             Office Equipment
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
+          <a href="#" className="nav-link" onClick={gamingItems}>
             Gaming
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
+          <a href="#" className="nav-link" onClick={cameraItems}>
             Camera
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
+          <a href="#" className="nav-link" onClick={upsItems}>
             UPS
           </a>
         </li>
       </ul>
       <div className="container py-5">
         <div className="row py-5">
-          {products.map((products) => (
-            <div key={products.id} className="col-md-4 py-3">
-              <img
-                src={products.img}
-                className="img-fluid"
-                style={{ width: "200px", height: "200px" }}
-                alt=""
-              />
-              <h4>{products.name}</h4>
-              <h5>Price: ${products.price}</h5>
-              <button
-                className="btn btn-info"
-                onClick={() =>
-                  dispatch({
-                    type: "ADD_TO_CART",
-                    id: products.id,
-                    products,
-                  })
-                }
-              >
-                Add to cart
-              </button>
-            </div>
-          ))}
+          {hotP
+            ? filterHotProducts.map((products) => (
+                <div key={products.id} className="col-md-4 py-4">
+                  <HotProducts products={products}></HotProducts>
+                </div>
+              ))
+            : selectCat.map((products) => (
+                <div key={products.id} className="col-md-3 py-4">
+                  <div>
+                    <div className="card text-center">
+                      <div className="card-body">
+                        <img
+                          src={products.img}
+                          className="card-img-top"
+                          alt=""
+                        />
+                        <p className="card-header">{products.name}</p>
+                        <p className="card-header">
+                          Price: ${products.price}
+                        </p>
+                      </div>
+                      <button
+                        className="btn btn-info btn-sm"
+                        onClick={() =>
+                          dispatch({
+                            type: "ADD_TO_CART",
+                            id: products.id,
+                            products,
+                          })
+                        }
+                      >
+                        Add to cart
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ))}
         </div>
       </div>
     </div>
---
+++ src/components/Products/HotProducts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HotProducts = (props) => {
+    return (
+        <div>
+            <h1>HPS</h1>
+        </div>
+    );
+};
+
+export default HotProducts;
